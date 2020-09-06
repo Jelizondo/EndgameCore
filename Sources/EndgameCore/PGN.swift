b8135382c7e63d47424837a2186eae84f312f0e6
Jorge <jelizondolevy.s@gmail.com>
2020-09-05 20:57:11 -0500

    Refactors PGNDecoder and PGN files.

--- Sources/EndgameCore/PGN.swift
+++ Sources/EndgameCore/PGN.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 public struct PGN {
-   
    public let event: String?
    public let site: String?
    public let date: String?
@@ -20,75 +19,6 @@ public struct PGN {
    public let result: String?
    public let moves: [String]
    public let rawValue: String
-   
-   public init(pgn: String) {
-      var keyValuePairs = [String:String]()
-      
-      // Parse metadata
-      let pgn = pgn.replacingOccurrences(of: "\r", with: "").replacingOccurrences(of: "\n\n", with: "\n")
-      
-      
-      let components = pgn.split(separator: "\n")
-      let metadata = components.filter { $0.contains("[") }
-      
-      for data in metadata {
-         let components = data.components(separatedBy: " \"")
-         let key = String(components[0].dropFirst())
-         let value = String(components[1].dropLast(2))
-         keyValuePairs[key] = value
-      }
-      
-      // Parse moves
-      
-      var aux = [String]()
-      let moves = pgn.components(separatedBy: "\n1.")
-                     .last!
-                     .replacingOccurrences(of: "\n", with: " ")
-                     .split(separator: " ")
-                     .dropLast()
-      
-      for move in moves {
-         let move = move.replacingOccurrences(of: "[0-9]*[.]",
-                                              with: "",
-                                              options: .regularExpression)
-         
-         guard !move.isEmpty else { continue }
-         
-         aux.append(String(move))
-      }
-      
-      // Initialize properties
-      rawValue = pgn
-      self.moves = aux
-      event = keyValuePairs["Event"]
-      site = keyValuePairs["Site"]
-      date = keyValuePairs["Date"]
-      round = keyValuePairs["Round"]
-      whitePlayer = keyValuePairs["White"]!
-      blackPlayer = keyValuePairs["Black"]!
-      result = keyValuePairs["Result"]
-      
-      if let elo = keyValuePairs["WhiteElo"] {
-         if elo.isEmpty {
-            whiteElo = nil
-         } else {
-            whiteElo = elo
-         }
-      } else {
-         whiteElo = nil
-      }
-      
-      if let elo = keyValuePairs["BlackElo"] {
-         if elo.isEmpty {
-            blackElo = nil
-         } else {
-            blackElo = elo
-         }
-      } else {
-         blackElo = nil
-      }
-      
-   }
    
 }
 
--- Sources/EndgameCore/PGNDecoder.swift
+++ Sources/EndgameCore/PGNDecoder.swift
@@ -25,14 +25,64 @@ public struct PGNDecoder {
          .dropFirst()
          .map { "[Event " + $0 }
       
-      var pgn: [PGN] = []
+      var pgns: [PGN] = []
       for rawPGN in rawPGNs {
-         pgn.append(PGN(pgn: rawPGN))
+         let pgn = decode(rawValue: rawPGN)
+         pgns.append(pgn)
       }
+      
+      return pgns
+   }
+   
+   public func decode(rawValue: String) -> PGN {
+      var keyValuePairs = [String:String]()
+      
+      // Parse metadata
+      let rawPGN = rawValue.replacingOccurrences(of: "\r", with: "").replacingOccurrences(of: "\n\n", with: "\n")
+      
+      
+      let components = rawPGN.split(separator: "\n")
+      let metadata = components.filter { $0.contains("[") }
+      
+      for data in metadata {
+         let components = data.components(separatedBy: " \"")
+         let key = String(components[0].dropFirst())
+         let value = String(components[1].dropLast(2))
+         keyValuePairs[key] = value
+      }
+      
+      // Parse moves
+      var aux = [String]()
+      let moves = rawPGN.components(separatedBy: "\n1.")
+         .last!
+         .replacingOccurrences(of: "\n", with: " ")
+         .split(separator: " ")
+         .dropLast()
+      
+      for move in moves {
+         let move = move.replacingOccurrences(of: "[0-9]*[.]",
+                                              with: "",
+                                              options: .regularExpression)
+         
+         guard !move.isEmpty else { continue }
+         
+         aux.append(String(move))
+      }
+      
+      
+      let pgn = PGN(event: keyValuePairs["Event"],
+                    site: keyValuePairs["Site"],
+                    date: keyValuePairs["Date"],
+                    round:  keyValuePairs["Round"],
+                    whitePlayer: keyValuePairs["White"]!,
+                    blackPlayer: keyValuePairs["Black"]!,
+                    whiteElo: keyValuePairs["WhiteElo"] == "" ? nil : keyValuePairs["WhiteElo"],
+                    blackElo: keyValuePairs["BlackElo"] == "" ? nil : keyValuePairs["BlackElo"],
+                    result: keyValuePairs["Result"],
+                    moves: aux,
+                    rawValue: rawPGN)
       
       return pgn
    }
    
 }
-
-
